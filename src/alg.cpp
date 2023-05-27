d0af34b3563803c2d75df9d6d1571fc73cfb40f2
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-05-28 02:00:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,20 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> BSTtree;
+    std::ifstream file(filename);
+    std::string result;
+    while (!file.eof()) {
+        char symbol = file.get();
+        if (symbol >= 'a' && symbol <= 'z') {
+           result+=symbol;
+        }
+        if(symbol>='A'&&symbol<='Z'){
+            symbol=symbol+('a'-'A');
+            BSTtree.add(result);
+            result+=symbol;
+        }
+    }
+    return BSTtree;
 }
