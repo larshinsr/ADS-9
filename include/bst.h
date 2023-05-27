b21c20ffe74b3c07324f52f9cca626b5b7669217
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-05-28 02:15:07 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,9 +1,10 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include<algorithm>
 template <typename T>
 class BST {
-   private:
+ private:
     struct NODE {
         T value;
         int count;
@@ -16,11 +17,11 @@ class BST {
             root->value = value;
             root->count = 1;
             root->left = root->right = nullptr;
-        } else if (root->value > value) {
+        } else if (root->value > value)
             root->left = addNODE(root->left, value);
-        } else if (root->value < value) {
+        else if (root->value < value)
             root->right = addNODE(root->right, value);
-        } else
+        else
             root->count += 1;
         return root;
     }
@@ -35,6 +36,7 @@ class BST {
         else
             return searchTree(root->left, value);
     }
+
    public:
     BST() : root(nullptr) {}
     void add(const T&) {
@@ -43,8 +45,7 @@ class BST {
     int depth() { return heightTree(root) - 1; }
     int search(const T& value) {
         NODE* current = serachTree(root, value);
-        if (current == nullptr)
-            return 0;
+        if (current == nullptr) return 0;
         else return current->count;
     }
 };
